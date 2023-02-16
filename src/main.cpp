f4a2fc0be81fa9f22bdce99db7123111a3702ed1
Aditya Jain <adityajain121108@gmail.com>
2023-02-16 12:37:28 +0530

    glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,43 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main()
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    
+    GLFWwindow* window = glfwCreateWindow(1600, 800, "urmo", NULL, NULL);
     if (!window)
     {
+        std::cout << "Welp... Window not working, glfwWindow not created\n";
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        //glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        //glBegin(GL_TRIANGLES);
+        //glVertex2f(0, 0);
+        //glVertex2f(0.3, 0.3);
+        //glVertex2f(0.5, 1);
+        //glEnd();
 
-        /* Poll for and process events */
+        //glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
